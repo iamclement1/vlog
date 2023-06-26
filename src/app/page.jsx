8479bcd0c26fabccc19d3d637780bc39e7fa0afe
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-06-26 09:17:24 +0100

    "feats: index page content"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,15 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>welcome</h1>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title} >Better design for your digital products</h1>
+        <p className={styles.description} >Turning your idea into reality. We bring together the teams from the global tech</p>
+        <button className={styles.button} >See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src="/img/hero.png" alt='hero-image' width={500} height={500}/>
+      </div>
     </div>
   )
 }
