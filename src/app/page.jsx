0a7de92112521760d0d5810b44decba64db471dd
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-06-26 10:12:38 +0100

    "update: imported button component"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/components/button/Button'
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
       <div className={styles.item}>
         <h1 className={styles.title} >Better design for your digital products</h1>
         <p className={styles.description} >Turning your idea into reality. We bring together the teams from the global tech</p>
-        <button className={styles.button} >See our works</button>
+        <Button url="/portfolio" text="See our works" />
       </div>
       <div className={styles.item}>
         <Image src="/img/hero.png" alt='hero-image' width={500} height={500}/>
