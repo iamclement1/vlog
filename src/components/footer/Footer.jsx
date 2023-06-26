7e6fd10638f99a9d3697c9fd28a01c771502e1b8
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-06-26 09:17:44 +0100

    "feats: footer page component content"

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
-   
+
    return (
       <Fragment>
          <div className={styles.container}>
-            &copy; 2023 vlog. All rights reserved
+            <div>
+               &copy; 2023 vlog. All rights reserved
+            </div>
+            <div className={styles.social} >
+               <Image src="/img/1.png" width={15} height={15} alt="facebook"  className={styles.icon}/>
+               <Image src="/img/2.png" width={15} height={15} alt="instagram" className={styles.icon} />
+               <Image src="/img/3.png" width={15} height={15} alt="twitter" className={styles.icon} />
+               <Image src="/img/4.png" width={15} height={15} alt="youtube" className={styles.icon} />
+
+            </div>
          </div>
+
 
       </Fragment>
    )
