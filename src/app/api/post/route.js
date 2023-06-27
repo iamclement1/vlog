28d9f5e5d67dca3d4743ec346b1dccc95ff6a308
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-06-27 12:13:21 +0100

    "feats: api for user and post"

---
+++ src/app/api/post/route.js
@@ -0,0 +1,18 @@
+import Post from "@/models/Post";
+import { connect } from "mongoose"
+import { NextResponse } from "next/server"
+
+export const GET = async (request) => {
+   //feth data
+
+   try {
+      await connect();
+
+      const posts = await Post.find();
+
+      return new NextResponse(JSON.stringify(posts), { status: 200 });
+   } catch (err) {
+      return new NextResponse("Database Error", { status: 500 });
+   }
+
+}
